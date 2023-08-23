78ce0341ccb8ea9867b5b3e446c415ba5e104a3d
Vinícius Tironi de Castilho <141459533+Vinicius-Tironi@users.noreply.github.com>
2023-08-23 17:11:40 -0300

    Add files via upload

---
+++ Cadastro.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(pdftools)
+
+pdf <- pdf_text("C://Users//Vinicius//Documents//Vinícius//Faculdade//Ferramentas Modelagem//Tarefa 3 - Cadastro R//cadastro.pdf")
+pdf <- str_split_1(pdf, "\\n")
+pdf
+
+#grepl: retorna a string desejada
+#grepl(string, variável)
+
+x <- pdf[grepl("[Nn]ome:", pdf)]
+
+#gsub: substitui a string desejada
+#gsub(string velha, string nova, variável)
+
+x <- gsub("[Nn]ome:", '' , x)
+x <- gsub("\\(.*\\)" , '' , x)
+
+x <- trimws(x, "both") # deleta espaços extra
+
+#str_extract(variável, "string") -> extrai a string desejada
+
+y <- pdf[grepl("[Nn]ome:", pdf)]
+y <- str_extract(y, "\\(.*\\)")
+
+y <- gsub("\\(\\w+ |\\)" , "" , y)
+
+z <- pdf[grepl("CEP:" , pdf)]
+z <- gsub("[^0-9 | -]" , "" , z)
+z <- trimws(z , "both")
